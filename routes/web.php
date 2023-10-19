ad10305d499ee10c6603250819400d5e0cc3665c
Ronald Emata Latina <ronald.emata.latina@gmail.com>
2023-10-19 09:32:34 +0800

    Updates

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users', [UserController::class, 'index']);
+Route::delete('delete-all', [UserController::class, 'removeMulti']);
